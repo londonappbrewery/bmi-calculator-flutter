5a2ee98b0f3e1d246b7ad6af11c08890ebe54017
Alexis <psialedev@gmail.com>
2020-12-04 16:12:56 -0600

    learned how to use flutter themes with ThemeData and using a default flutter theme with extra tweeks using copyWith

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
   }
