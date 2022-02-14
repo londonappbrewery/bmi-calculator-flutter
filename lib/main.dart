f0d804d9f4deea31846adc06c52af440e52a0bc9
ideal_lust3r <s1skyblue1s@gmail.com>
2022-02-14 19:53:32 +0900

    Color Error
    Modify to another code that changes because the primary color does not work in the 22.02 Flutter version.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: Theme.of(context).copyWith(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: const Color(0xFF0A0E21),
+        ),
+      ),
       home: InputPage(),
     );
   }
