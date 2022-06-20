98b35cb8bdac080083dcca959feec5c038356314
Arthur Gusmao <arthur.cabj@hotmail.com>
2022-06-20 09:43:13 -0300

    feat: new page file for home screen

---
+++ lib/input_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+    );
+  }
+}
